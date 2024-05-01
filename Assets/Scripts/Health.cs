8063652dfb53dc8226f6ba87840e4f717631a14d
Vlad Goloyan <vladimir.goloyan@helpsystems.com>
2024-05-01 15:44:09 +0400

    Initial commit

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private bool dead;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+
+        }
+        else
+        {
+            if (!dead)
+            {
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+                SceneManager.LoadScene(2);
+            }
+        }
+    }
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+}
